6ec20538f609560e9b0fe239a9e9d4ebc2e3b4b3
JJLin <jjlin_06120102@foxmail.com>
2022-11-28 21:44:48 +0800

    Finish checking out whether the user access the index page again

--- src/main/java/com/example/fromzerotoexpert/controller/IndexController.java
+++ src/main/java/com/example/fromzerotoexpert/controller/IndexController.java
@@ -28,9 +28,10 @@ public class IndexController {
     @Value("${project.index.cookie.value}")
     private String cookieValue;
     /**再次访问index页重定向路径*/
-    @Value("${project.index.redirect.path}")
+    @Value("${project.index.redirectPath}")
     private String redirectPath;
 
+    /**访问index页*/
     @GetMapping("FromZerotoExpert")
     public String accessFirstly(
             HttpServletRequest request,
@@ -38,7 +39,7 @@ public class IndexController {
     ) throws IOException {
         Cookie[] cookies = request.getCookies();
         //请求携带cookie
-        if(cookies.length == 0 || cookies == null) {
+        if(cookies.length != 0 || cookies != null) {
             for(Cookie cookie : cookies) {
                 if(!cookie.getName().equals(cookieName)) continue;
                 //请求带cookie且cookie信息正确 重定向到再次登录index页
@@ -49,10 +50,11 @@ public class IndexController {
         }
         //请求不携带cookie 发送cookie
         Cookie cookie = new Cookie(cookieName, cookieValue);
+        cookie.setMaxAge(2);
         response.addCookie(cookie);
         return "嗨，欢迎您来到 " + nickname;
     }
-
+    /**再次访问index页的重定向映射*/
     @GetMapping("accessIndexAgain")
     public String accessAgain() {
         return "嗨，欢迎您再次来到 " + nickname;
